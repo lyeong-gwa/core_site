82361b44451da355d3d5c95773706f934edee116
lyeong-gwa <wjdgmll77@naver.com>
2021-08-26 15:46:34 +0900

    d

--- public/js/core_script.js
+++ public/js/core_script.js
@@ -135,10 +135,12 @@ document.querySelector("#info").addEventListener("submit",bansubmit);
 	}else if(chkprofile==''){
 		alert("코어창 상태이미지를 업로드해주세요!");
 		A.preventDefault();
-	}
-	if(chkprofile_count_limit>15){
+	}else if(chkprofile_count_limit>15){
 		alert("이미지파일은 최대 15개로 제한합니다");
 		A.preventDefault();
+	}else{
+		document.getElementById('post_button').disabled = true;
+		LoadingWithMask();
 	}
  }
 
@@ -236,7 +238,36 @@ function classification_job_f(choice_class){
 	}
 	skill_table(document.querySelector("select[name=job]"));
 }
+function LoadingWithMask() {
+    //화면의 높이와 너비를 구합니다.
+    var maskHeight = $(document.body).height();
+    var maskWidth  = window.document.body.clientWidth;
+     
+    //화면에 출력할 마스크를 설정해줍니다.
+    var mask       ="<div id='mask' style='position:absolute; z-index:9000; background-color:#000000; display:none; left:0; top:0;'></div>";
+      
+    let loadingImg ="<img src='./Spinner.gif' style='position: relative; display: block; margin: 0px auto;'/>";
+  
+    //화면에 레이어 추가
+    $('body')
+        .append(mask)
+        
+    //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채웁니다.
+    $('#mask').css({
+            'width' : maskWidth
+            ,'height': maskHeight
+            ,'opacity' :'0.3'
+    });
+  
+    //마스크 표시
+    $('#mask').show();  
+  
+    //로딩중 이미지 표시
+    document.getElementById('loadingImg').innerHTML=loadingImg;
+}
 
+
+출처: https://kkamikoon.tistory.com/168 [컴퓨터를 다루다]
 window.onpageshow=function(event){
 	skill_table(document.querySelector("select[name=job]"));
 }
